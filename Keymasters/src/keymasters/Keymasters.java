ff1f01ac09753d68a3b0df5973341a6b6247236b
A S U S <A S U S@Hbdvddndl>
2023-12-07 15:30:54 +0800

    Revert "Hbdvddndl testing"
    This reverts commit 478337898fc3a720cbea0b81ef5284a2c89b78a8.

--- Keymasters/src/keymasters/Keymasters.java
+++ Keymasters/src/keymasters/Keymasters.java
@@ -6,7 +6,6 @@ public class Keymasters {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("This is newly added words from branch.");
-        System.out.println("This is Hbdvddndl");
     }
     
 }
